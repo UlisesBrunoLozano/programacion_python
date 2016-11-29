2ebd5b5fbc5ae96b07b70cfe9ed8e80a4ce90adb
Ulises Bruno Lozano <yongork@gmail.com>
2016-11-29 09:27:07 -0600

    Add files via upload

---
+++ Laboratorio 3/tarea3.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import math as m
+import numpy as np
+a=input("Introduce la latitud de tu punto inicial: ")
+b=input("Introduce la longitud de tu punto inicial: ")
+c=input("Introduce la latitud del punto final: ")
+d=input("Introduce la longitud del punto final: ")
+def distancia(t1,g1,t2,g2):
+    Pi=3.1415
+    t1=(a*Pi/180)
+    g1=(b*Pi/180)
+    t2=(c*Pi/180)
+    g2=(d*Pi/180)
+    sins=m.sin(t1)*m.sin(t2)+m.cos(t1)*m.cos(t2)*m.cos(g1-g2)
+    arrow=6371.01*m.acos(sins)
+    return arrow
+    print arrow
+
+output=distancia(a,b,c,d)
+print output
