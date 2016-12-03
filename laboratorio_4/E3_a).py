68472fc831fe20bc2d41e3f24ed4c0e7a40845f0
Ulises Bruno Lozano <yongork@gmail.com>
2016-12-03 02:21:52 -0600

    Add files via upload

---
+++ laboratorio_4/E3_a).py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+t=np.linspace(0,2*np.pi,100)
+x=np.cos(t)*np.cos(t)*np.cos(t)
+y=np.sin(t)*np.sin(t)*np.sin(t)
+plt.plot(t,x,linewidth=3,color='b',label='t(x)')
+plt.plot(t,y,linewidth=2,color='r',label='t(y)')
+plt.legend('xy')
+plt.title('')
+plt.xlabel('t')
+plt.ylabel('X(t)&Y(t)')
+plt.grid(True)
+plt.show()
+plt.savefig('grafica3a.png')
