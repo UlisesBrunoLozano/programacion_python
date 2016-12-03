108dcc1286dedb8dfa7601ef0e2a20945b73dd7c
Ulises Bruno Lozano <yongork@gmail.com>
2016-12-03 14:45:12 -0600

    Update Ejercicio 4.py

--- laboratorio_2/Ejercicio 4.py
+++ laboratorio_2/Ejercicio 4.py
@@ -2,17 +2,12 @@
 #Ulises Bruno Lozano
 #25/11/16
 import matplotlib.pyplot as plt
-x=input("Ingrese los lados de un poligono regular: ")
-for x in range(0,x):
-    if x%2==0:
-      p0=[0,0]
-      p1=[x,0]
-      p2=[x,x]
-      p3=[0,0]
-      n=[[p0],[p1],[p2],[p3]]
-    print n
-    plt.plot(n)
-    plt.show()
-
-salida=n(x)
-print salida
+import numpy as np
+a=input("Ingrese los lados de un poligono regular: ")
+x=np.linspace(0,1,100)
+lista=[]
+for i in range(a):
+    x1=[0,i]
+    y1=[i,0]
+    lista.append(x1+y1)
+print lista
